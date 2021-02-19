71b10e5ca7c614e9e4cf9620a347e0d2eeead749
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-02-19 22:11:55 +0900

    Add files via upload

---
+++ BOJ/Main_백준_3053_택시기하학.java
@@ -0,0 +1,15 @@
+package add;
+
+import java.util.Scanner;
+
+public class Main_백준_3053_택시기하학 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		
+		System.out.println(r*r*Math.PI);
+		System.out.println(r*r*2);
+		
+		
+	}
+}
