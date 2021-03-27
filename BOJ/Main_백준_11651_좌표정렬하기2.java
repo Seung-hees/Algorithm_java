60f579735823f1dd070065449776a15b8e26867c
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-03-28 00:38:27 +0900

    Add files via upload

---
+++ BOJ/Main_백준_11651_좌표정렬하기2.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main_백준_11651_좌표정렬하기2 {
+	static class point {
+		int x;
+		int y;
+
+		public point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		point[] arr = new point[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[i] = new point(x, y);
+		}
+
+		Arrays.sort(arr, new Comparator<point>() {
+
+			@Override
+			public int compare(point o1, point o2) {
+				if (o1.x == o2.x) {
+					return o1.y - o2.y;
+				}
+				return o1.x - o2.x;
+			}
+
+		});
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i].x + " " + arr[i].y);
+		}
+
+	}// end of main
+
+}// end of class
