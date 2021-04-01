b02c1e3420bc497a5ef5b7093aebdbf8e5e3606c
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-04-01 17:08:19 +0900

    Create Solution_SWEA_1940_가랏!RC카!.java

---
+++ SWEA/Solution_SWEA_1940_가랏!RC카!.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int sum = 0;
+			int v = 0;
+			for (int i = 0; i < N; i++) {
+				int c = sc.nextInt();
+				switch (c) {
+				case 0:
+					break;
+				case 1:
+					v += sc.nextInt();
+					break;
+				case 2:
+					v -= sc.nextInt();
+					break;
+				}
+				if(v<0) v=0;
+				sum += v;
+			}
+			System.out.println("#" + tc + " " + sum);
+		}
+
+		sc.close();
+
+	}
+
+}
