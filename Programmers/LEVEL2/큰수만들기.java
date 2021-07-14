363fe9c4355197789277d3d2519c8dd4add8c03b
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-07-14 23:08:21 +0900

    greedy

---
+++ Programmers/LEVEL2/큰수만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        
+        int idx = 0;
+        int max = 0;// 가장 큰 맨 앞자리 
+        for(int i=0; i<number.length()-k; i++){
+            max = 0;
+            for(int j=idx; j<=i+k; j++){
+                if(max < number.charAt(j)-'0'){
+                    max = number.charAt(j)-'0';
+                    idx = j+1;
+                }
+            }
+            answer.append(max);
+        }
+        return answer.toString();
+    }
+
+}
