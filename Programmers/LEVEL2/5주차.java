58b3b529313dd90d3c9873711303e123325daec6
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-09-02 21:58:39 +0900

    위클리-챌린지, 중복순열

---
+++ Programmers/LEVEL2/5주차.java
@@ -0,0 +1,28 @@
+class Solution {
+    private static String alpha[] = {"A","E","I","O","U"};
+    private static int cnt,ans = 0; 
+    
+    public int solution(String word) {
+        
+        permu(0,"",word);
+        return ans;
+    }
+    
+    private static void permu(int idx, String s, String word){
+        if(idx == 5){
+            return;
+        }
+        // 앞 자리에서 부터 채워나감.
+        for(int i=0; i<alpha.length; i++){
+            String str = s + alpha[i];
+            cnt++;
+            if(str.equals(word)){
+                ans = cnt;
+                return;
+            }
+            permu(idx + 1, str, word);
+        }
+        
+    }
+    
+}
