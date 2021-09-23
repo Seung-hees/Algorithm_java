78670017e9a8f00df18a4e35ae805dbb4fe9b640
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-09-23 23:55:20 +0900

    Update Main_백준_16234_인구이동.java

--- BOJ/Main_백준_16234_인구이동.java
+++ BOJ/Main_백준_16234_인구이동.java
@@ -1,1 +1,29 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_16234_인구이동 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+		
+	}// end of main
+
+}// end of class
