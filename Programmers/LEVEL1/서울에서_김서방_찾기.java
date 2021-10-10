f625e6cf0623d0f87526b1297afa7c5efa7aaf1f
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-10-10 23:08:20 +0900

    string

---
+++ Programmers/LEVEL1/서울에서_김서방_찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "김서방은 ";
+        int idx = 0;
+        for(int i=0; i<seoul.length ; i++){
+            if(seoul[i].equals("Kim")){
+                idx = i;
+                break;
+            }
+        }
+        return answer+idx+"에 있다";
+    }
+}
