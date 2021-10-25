fc40ae0c16827de0f0e3368eb0c0fab104e039c7
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-10-25 22:53:53 +0900

    Update Main_백준_1002_터렛.java

--- BOJ/Main_백준_1002_터렛.java
+++ BOJ/Main_백준_1002_터렛.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main_백준_1002_터렛 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int TC = Integer.parseInt(br.readLine());
 
