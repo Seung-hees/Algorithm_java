0d8fc50ce70182027c203fc0c418bbf28b9fedf8
Seunghee Lee <59682953+Seung-hees@users.noreply.github.com>
2021-11-09 23:06:29 +0900

    Update 없는숫자더하기.py

--- Programmers/LEVEL1/없는숫자더하기.py
+++ Programmers/LEVEL1/없는숫자더하기.py
@@ -3,3 +3,25 @@ def solution(numbers):
     for i in numbers:
         answer-=i
     return answer
+
+def solution(numbers):
+    answer = 0
+    
+    for i in range(0,10) :
+        if i not in numbers :
+            answer+=i
+    return answer
+
+def solution(numbers):
+    answer = 0
+    check = [0] * 10
+    for i in numbers :
+        check[i] += 1
+    for i in check :
+        if i == 0 :
+            answer += check.index(i)
+            check[check.index(i)] = 1
+    return answer
+
+def solution(numbers):
+    return 45-sum(numbers)
